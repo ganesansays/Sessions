05b6f8fc724b6f651ac40449c7b14f70ae5b5299
GaneshRamakrishnan <ganesh.ramakrishnan01@gmail.com>
2014-08-10 00:53:23 +0530

    raise invoice

---
+++ Session2/Session2/RaiseInvoice.cs
@@ -0,0 +1,51 @@
+﻿using BusinessEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Session2
+{
+    class RaiseInvoice
+    {
+       public void AddInvoice(Invoice invoice)
+      {
+        Repository.InvoiceList.Add(invoice);
+      }
+
+       public Invoice ReadInvoice(int Id)
+        {
+          return Repository.InvoiceList.Find(
+                inv => inv.Id == Id
+            );
+        }
+
+        public void UpdateInvoice(Invoice invoice)
+        {
+            if (invoice == null) return;
+
+            Invoice inv = Repository.InvoiceList.Find(
+                  i => i.Id == invoice.Id
+                );
+
+            inv.CustomerId = invoice.CustomerId;
+            inv.SaleId = invoice.SaleId;
+            inv.TotalAmount = invoice.TotalAmount;
+        }
+
+        public void DeleteInvoice(int id)
+        {
+            Invoice inv = Repository.InvoiceList.Find(
+                i => i.Id == i.Id
+                );
+
+            if (inv != null)
+            {
+                Repository.InvoiceList.Remove(inv);
+            }
+        }
+    }
+
+}
