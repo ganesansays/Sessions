b559f043a680cb4689818072332f8e4a819b136c
ganegee <ganesansays@gmail.com>
2014-08-10 00:10:15 +0530

    Changing Product to Repository

---
+++ Session2/Session2/ProductOperation.cs
@@ -0,0 +1,51 @@
+﻿using BusinessEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session2
+{
+    class ProductOperation
+    {
+        public void CreateProduct(Product product)
+        {
+            Repository.Products.Add(product);
+        }
+
+        public Product FetchProduct(int Id)
+        {
+            return Repository.Products.Find(
+                  prod => prod.Id == Id
+              );
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            if (product == null) return;
+
+            Product prod = Repository.Products.Find(
+                    c => c.Id == product.Id
+                );
+
+            if (prod != null)
+            {
+                prod.Name = product.Name;
+                prod.Rate = product.Rate;
+            }
+        }
+
+        public void DeleteProduct(int Id)
+        {
+            Product prod = Repository.Products.Find(
+                    p => p.Id == Id
+                );
+
+            if (prod != null)
+            {
+                Repository.Products.Remove(prod);
+            }
+        }
+    }
+}
--- Session2/Session2/Program.cs
+++ Session2/Session2/Program.cs
@@ -19,44 +19,7 @@ namespace Session2
         //CRUD SalesPerson
         //CRUD Product
 
-        public void CreateProduct(Product product)
-        {
-            Products.Add(product);
-        }
-
-        public Product FetchProduct(int Id)
-        {
-              return Products.Find(
-                    prod => prod.Id == Id
-                );
-        }
-
-        public void UpdateProduct(Product product)
-        {
-            if (product == null) return;
-
-            Product prod = Products.Find(
-                    c => c.Id == product.Id
-                );
-
-            if (prod != null)
-            {
-                prod.Name = product.Name;
-                prod.Rate = product.Rate;
-            }
-        }
-
-        public void DeleteProduct(int Id)
-        {
-            Product prod = Products.Find(
-                    p => p.Id == Id
-                );
-
-            if (prod != null)
-            {
-                Products.Remove(prod);
-            }
-        }
+        
 
 
         //Create Sale (Product/Customer/Salesperson)
