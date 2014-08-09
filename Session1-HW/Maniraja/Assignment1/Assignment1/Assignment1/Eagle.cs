861f4f097ed423cadc5893c34e7a8db8ee3bb03d
ganegee <ganesansays@gmail.com>
2014-08-10 00:07:51 +0530

    Git and Eagle from Maniraja solution

---
+++ Session1-HW/Maniraja/Assignment1/Assignment1/Assignment1/Eagle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentClasses
+{
+    class Eagle : Animal, IFlyable
+    {
+        public Eagle(int age, int weight) 
+            : base (age, weight)
+        {
+        
+        }
+
+        public override void Feed()
+        {
+            Console.WriteLine("Feed Eagle...");
+            printAgeAndWeight();
+        }
+
+        public override void makeSound()
+        {
+            Console.WriteLine("Eagle sound ...");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("I am flying ...");
+        }
+    }
+}
