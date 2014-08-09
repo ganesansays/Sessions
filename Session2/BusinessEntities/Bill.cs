961462194971365c1cb70d480faeceb8ad85494e
ganegee <ganesansays@gmail.com>
2014-08-09 23:55:30 +0530

    Business Entities

---
+++ Session2/BusinessEntities/Bill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class Bill
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+        public int CustomerId { get; set; }
+        public decimal AmountPaid { get; set; }
+
+        public Bill(int Id, int InvoiceId, int CustomerId, decimal AmountPaid)
+        {
+            this.Id = Id;
+            this.InvoiceId = InvoiceId;
+            this.CustomerId = CustomerId;
+            this.AmountPaid = AmountPaid;
+        }
+    }
+}
---
+++ Session2/BusinessEntities/Sale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public int SalesPersonId { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Discount { get; set; }
+
+        public Sale(int Id, int SalesPersonId, int CustomerId, int ProductId, int Quantity, int Discount)
+        {
+            this.Id = Id;
+            this.SalesPersonId = SalesPersonId;
+            this.CustomerId = CustomerId;
+            this.ProductId = ProductId;
+            this.Quantity = Quantity;
+            this.Discount = Discount;
+        }
+
+
+    }
+}
