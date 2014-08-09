3d97c7ed2993ae06f351b3c94e0eefc4e619eae2
ganegee <ganesansays@gmail.com>
2014-08-10 00:00:13 +0530

    Adding Repository

--- Session2/Session2/Program.cs
+++ Session2/Session2/Program.cs
@@ -14,59 +14,7 @@ namespace Session2
              
         }
 
-        public List<SalesPerson> SalesPerson;
-        public List<Customer> Customers;
-        public List<Product> Products;
-        public List<Sale> SaleList;
-        public List<Invoice> InvoiceList;
-        public List<Bill> Bills;
-
-        public void CreateCustomer(Customer customer)
-        {
-            Customers.Add(customer);
-        }
-
-        public Customer ReadCustomer(int Id)
-        {
-            /*for (int i = 0; i < Customers.Count; i++)
-            {
-                Customer cust = Customers[i];
-
-                if (cust.Id == Id) return cust;
-            }*/
-
-            return Customers.Find(
-                    cust => cust.Id == Id
-                );
-        }
-
-        public void UpdateCustomer(Customer customer)
-        {
-            if (customer == null) return;
-            
-            Customer cust = Customers.Find(
-                    c => c.Id == customer.Id
-                );
-
-            if (cust != null)
-            {
-                cust.Name = customer.Name;
-                cust.ContactNumber = customer.ContactNumber;
-            }
-        }
-
-        public void DeleteCustomer(int Id)
-        {
-            Customer cust = Customers.Find(
-                    c => c.Id == Id
-                );
-
-            if (cust != null)
-            {
-                Customers.Remove(cust);
-            }
-        }
-
+        
         //CRUD (Create/Read/Update/Delete) Customer
         //CRUD SalesPerson
         //CRUD Product
