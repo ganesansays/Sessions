cb0ac8fae6f4e810cbbb7dc41acd05b9074f6215
simaniraja <simaniraja@gmail.com>
2014-08-15 15:18:16 +0530

    HandleSalesPerson

--- Session2/Session2/Program.cs
+++ Session2/Session2/Program.cs
@@ -39,6 +39,7 @@ namespace Session2
                     case 2:
                         break;
                     case 3:
+                        HandleSalesPerson();
                         break;
                     case 4:
                         break;
@@ -153,6 +154,115 @@ namespace Session2
                         return;
                 }
             }
+        }
+        private static void HandleSalesPerson()
+        {
+            //HandleSaleperson
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("SALES PERSON");
+                Console.WriteLine("What operation you want to perform?");
+                Console.WriteLine("[1]: Create");
+                Console.WriteLine("[2]: Fetch");
+                Console.WriteLine("[3]: Update");
+                Console.WriteLine("[4]: Delete");
+                Console.WriteLine("[5]: Go to main menu");
+
+                string choiceString = Console.ReadLine();
+                int choice = 0;
+                if (!Int32.TryParse(choiceString, out choice))
+                {
+                    Console.WriteLine("Please enter a valid choice ...");
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.Write("Enter SalesPerson Id: ");
+                        string spIdInput = Console.ReadLine();
+                        Console.Write("Enter SalesPerson Name: ");
+                        string spNameInput = Console.ReadLine();
+                        Console.Write("Enter SalesPerson Number: ");
+                        string spContactNumberInput = Console.ReadLine();
+
+                        int id = 0;
+                        Int32.TryParse(spIdInput, out id);
+
+                        SalesPerson salePersonObject = new SalesPerson(id, spNameInput, spContactNumberInput);
+                        SalesPersonOperation.CreateSalesPerson(salePersonObject);
+                        Console.WriteLine("Sucessfully created SP  ... press anykey to continue ...");
+                        Console.ReadLine();
+                        break;
+                    
+                    case 2:
+                        Console.Clear();
+                        Console.Write("Enter SalesPerson Id: ");
+                        spIdInput = Console.ReadLine();
+                        id = 0;
+                        Int32.TryParse(spIdInput, out id);
+
+                        salePersonObject = SalesPersonOperation.FetchSalesPerson(id);
+
+
+                        if (salePersonObject != null)
+                        {
+                            Console.WriteLine("SP Name:" + salePersonObject.Name);
+                        }
+                        else
+                        {
+                            Console.WriteLine("SP doesnt exist");
+                        }
+
+                        Console.WriteLine("press anykey to continue ...");
+                        Console.ReadLine();
+                        break;
+
+                    case 3:
+                        //update
+                        Console.Clear();
+                        Console.Write("Enter SalePerson Id: ");
+                        spIdInput = Console.ReadLine();
+                        Console.Write("Enter SalePerson Name: ");
+                        spNameInput = Console.ReadLine();
+                        Console.Write("Enter Contact Number: ");
+                        spContactNumberInput = Console.ReadLine();
+
+                        id = 0;
+                        Int32.TryParse(spIdInput, out id);
+
+                        salePersonObject = new SalesPerson(id, spNameInput, spContactNumberInput);
+
+                        if (salePersonObject != null)
+                        {
+                            SalesPersonOperation.UpdateSalesPerson(salePersonObject);
+                            Console.WriteLine("Sucessfully updated SP... press anykey to continue ...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("SP doesnt exist");
+                        }
+
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.Write("Enter Customer Id: ");
+                        spIdInput = Console.ReadLine();
+                        id = 0;
+                        Int32.TryParse(spIdInput, out id);
+                        SalesPersonOperation.DeleteSalesPerson(id);
+                        Console.WriteLine("Sucessfully deleted SP ... press anykey to continue ...");
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        return;
+                }
+            }
+
+
         }
     }
 }
