2550ff66c2cfa4173f3985facb220ea480a26c46
ganegee <ganesansays@gmail.com>
2014-08-15 15:29:37 +0530

    Merging Manis code

--- Session2/Session2/Program.cs
+++ Session2/Session2/Program.cs
@@ -37,11 +37,13 @@ namespace Session2
                         HandleCustomerInformation();
                         break;
                     case 2:
+                        HandleProductInformation();
                         break;
                     case 3:
                         HandleSalesPerson();
                         break;
                     case 4:
+                        HandleSalesInformation();
                         break;
                     case 5:
                         break;
@@ -54,6 +56,136 @@ namespace Session2
                         continue;
                 }
             }
+        }
+
+        private static void HandleSalesInformation()
+        {
+            string v_userMenuChoice;
+            int v_userChoice = 0;
+
+            int i_SalesID;
+            int i_SalesPersonID;
+            int i_CustomerID;
+            int i_SalesProductID;
+            int i_SalesQuantity;
+            decimal i_SalesDiscount;
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose the operation you would like to perform:");
+                Console.WriteLine("[1]: Create Sales");
+                Console.WriteLine("[2]: Retrieve Sales ");
+                Console.WriteLine("[3]: Update Sales");
+                Console.WriteLine("[4]: Delete Sales");
+                Console.WriteLine("[5]: Go to Main Menu");
+
+                v_userMenuChoice = Console.ReadLine();
+
+                if (!Int32.TryParse(v_userMenuChoice, out v_userChoice))
+                {
+                    Console.WriteLine("Please choose a valid option...");
+                }
+
+                switch (v_userChoice)
+                {
+                    case 1:
+                        Console.Clear();
+
+                        Console.Write("Enter Sales ID:");
+                        i_SalesID = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Person ID:");
+                        i_SalesPersonID = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Customer ID:");
+                        i_CustomerID = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Product ID:");
+                        i_SalesProductID = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Quantity:");
+                        i_SalesQuantity = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Discount:");
+                        i_SalesDiscount = Convert.ToDecimal(Console.ReadLine());
+
+                        Sale o_sales = new Sale(i_SalesID, i_SalesPersonID, i_CustomerID, i_SalesProductID, i_SalesQuantity, i_SalesDiscount);
+                        SalesOperations.CreateSale(o_sales);
+                        Console.WriteLine("Successful! Sales Added.Press any key to continue...");
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+
+                        Console.Write("Enter Sales ID:");
+                        i_SalesID = Convert.ToInt32(Console.ReadLine());
+                        o_sales = SalesOperations.FetchSale(i_SalesID);
+
+                        if (o_sales != null)
+                        {
+                            Console.WriteLine("Sales ID : {0}", o_sales.Id);
+                            Console.WriteLine("Sales Person ID : {0}", o_sales.SalesPersonId);
+                            Console.WriteLine("Sales Customer ID : {0}", o_sales.CustomerId);
+                            Console.WriteLine("Sales Product ID : {0}", o_sales.ProductId);
+                            Console.WriteLine("Sales Quantity : {0}", o_sales.Quantity);
+                            Console.WriteLine("Sales Discount : {0}", o_sales.Discount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("The mentioned sales id {0} does not exist", i_SalesID);
+                        }
+
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadLine();
+                        break;
+
+
+                    case 3:
+                        Console.Clear();
+
+                        Console.Write("Enter Sales ID:");
+                        i_SalesID = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Person ID:");
+                        i_SalesPersonID = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Customer ID:");
+                        i_CustomerID = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Product ID:");
+                        i_SalesProductID = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Quantity:");
+                        i_SalesQuantity = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Enter Sales Discount:");
+                        i_SalesDiscount = Convert.ToDecimal(Console.ReadLine());
+
+                        o_sales = new Sale(i_SalesID, i_SalesPersonID, i_CustomerID, i_SalesProductID, i_SalesQuantity, i_SalesDiscount);
+                        SalesOperations.UpdateSale(o_sales);
+                        Console.WriteLine("Successful! Sales Updated.Press any key to continue...");
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+
+                        Console.Write("Enter Sales ID:");
+                        i_SalesID = Convert.ToInt32(Console.ReadLine());
+
+                        SalesOperations.DeleteSale(i_SalesID);
+                        Console.WriteLine("Successful! Sales Deleted.Press any key to continue...");
+                        Console.ReadLine();
+                        break;
+
+                    case 5:
+                        return;
+                }
+            }
+
+
         }
 
         private static void HandleCustomerInformation()
@@ -102,8 +234,8 @@ namespace Session2
                         Int32.TryParse(idInput, out id);
                         cust = CustomerOperation.FetchCustomer(id);
 
-                        if (cust != null) 
-                        { 
+                        if (cust != null)
+                        {
                             Console.WriteLine(cust.Name);
                         }
                         else
@@ -128,8 +260,8 @@ namespace Session2
 
                         cust = new Customer(id, name, contactNumber);
 
-                        if (cust != null) 
-                        { 
+                        if (cust != null)
+                        {
                             CustomerOperation.UpdateCustomer(cust);
                             Console.WriteLine("Sucessfully updated ... press anykey to continue ...");
                         }
@@ -155,6 +287,114 @@ namespace Session2
                 }
             }
         }
+
+        private static void HandleProductInformation()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("What operation you want to perform?");
+                Console.WriteLine("[1]: Create");
+                Console.WriteLine("[2]: Fetch");
+                Console.WriteLine("[3]: Update");
+                Console.WriteLine("[4]: Delete");
+                Console.WriteLine("[5]: Go to main menu");
+
+                string choiceString = Console.ReadLine();
+                int choice = 0;
+                if (!Int32.TryParse(choiceString, out choice))
+                {
+                    Console.WriteLine("Please enter a valid choice ...");
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.Write("Enter Product Id: ");
+                        string idInput = Console.ReadLine();
+                        Console.Write("Enter Product Name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter Product Rate: ");
+                        string rateinput = Console.ReadLine();
+
+                        int id = 0;
+                        Int32.TryParse(idInput, out id);
+
+                        decimal rate = 0.00M;
+                        decimal.TryParse(rateinput, out rate);
+
+                        Product Prod = new Product(id, name, rate);
+
+                        ProductOperation.CreateProduct(Prod);
+
+                        Console.WriteLine("Sucessfully created ... press anykey to continue ...");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.Write("Enter Product Id: ");
+                        idInput = Console.ReadLine();
+                        id = 0;
+                        Int32.TryParse(idInput, out id);
+                        Prod = ProductOperation.FetchProduct(id);
+
+                        if (Prod != null)
+                        {
+                            Console.WriteLine(Prod.Name);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Product doesnt exist");
+                        }
+
+                        Console.WriteLine("press anykey to continue ...");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.Write("Enter Product Id: ");
+                        idInput = Console.ReadLine();
+                        Console.Write("Enter Product Name: ");
+                        name = Console.ReadLine();
+                        Console.Write("Enter Product Rate: ");
+                        rateinput = Console.ReadLine();
+
+                        id = 0;
+                        Int32.TryParse(idInput, out id);
+
+                        rate = 0.00M;
+                        decimal.TryParse(rateinput, out rate);
+                        Prod = new Product(id, name, rate);
+
+                        if (Prod != null)
+                        {
+                            ProductOperation.UpdateProduct(Prod);
+                            Console.WriteLine("Sucessfully updated ... press anykey to continue ...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Product doesnt exist");
+                        }
+
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.Write("Enter Product Id: ");
+                        idInput = Console.ReadLine();
+                        id = 0;
+                        Int32.TryParse(idInput, out id);
+                        ProductOperation.DeleteProduct(id);
+                        Console.WriteLine("Sucessfully deleted ... press anykey to continue ...");
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        return;
+                }
+            }
+        }
+
         private static void HandleSalesPerson()
         {
             //HandleSaleperson
@@ -196,7 +436,7 @@ namespace Session2
                         Console.WriteLine("Sucessfully created SP  ... press anykey to continue ...");
                         Console.ReadLine();
                         break;
-                    
+
                     case 2:
                         Console.Clear();
                         Console.Write("Enter SalesPerson Id: ");
@@ -261,8 +501,7 @@ namespace Session2
                         return;
                 }
             }
-
-
         }
     }
 }
+        
